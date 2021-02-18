94799e972e5c6f3a79d2d5eb31819f9e95e239f4
ZapAnton <wc3ft2@gmail.com>
2021-02-18 10:19:13 +0300

    Added seperate 'get_ssid' function

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_SSID_LENGTH 32
+
 void print_version(void) { printf("wifi_password %s\n", PROJECT_VERSION); }
 
 void print_help(void) {
@@ -14,22 +16,27 @@ void print_help(void) {
            PROJECT_DESCRIPTION);
 }
 
-void fetch_wifi(void) {
+void get_ssid(char *ssid_str) {
     FILE *nmcli_process = popen(
         "nmcli -t -f active,ssid dev wifi | grep yes | sed 's/yes://'", "r");
     if (nmcli_process == NULL) {
         fprintf(stderr, "Failed to open nmcli process");
         exit(1);
     }
-    char ssid[40] = {0};
-    const char *const fgets_result = fgets(ssid, 40, nmcli_process);
+    const char *const fgets_result =
+        fgets(ssid_str, MAX_SSID_LENGTH, nmcli_process);
     // TODO: Remove the newline from fgets
     if (fgets_result == NULL) {
         fprintf(stderr, "Failed to read from nmcli process");
         exit(1);
     }
-    puts(ssid);
     pclose(nmcli_process);
+}
+
+void fetch_wifi(void) {
+    char ssid[MAX_SSID_LENGTH] = {0};
+    get_ssid(ssid);
+    puts(ssid);
 }
 
 int main(int argc, char **argv) {
