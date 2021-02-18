1410f10badb4e56a8fe5ad00ef7f0b0efe35b3e5
ZapAnton <wc3ft2@gmail.com>
2021-02-18 11:21:44 +0300

    Added password fetching via nmcli

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_SSID_LENGTH 32
+#define MAX_SSID_LENGTH 33
+#define MAX_PASSWORD_LENGTH 65
 
 void print_version(void) { printf("wifi_password %s\n", PROJECT_VERSION); }
 
@@ -33,10 +34,35 @@ void get_ssid(char *ssid_str) {
     pclose(nmcli_process);
 }
 
+void get_password(char *password_str, const char *const ssid) {
+    char nmcli_command[60 + MAX_SSID_LENGTH + 1];
+    sprintf(nmcli_command,
+            "nmcli -s -g 802-11-wireless-security.psk connection show '%s'",
+            ssid);
+    FILE *nmcli_process = popen(nmcli_command, "r");
+    if (nmcli_process == NULL) {
+        fprintf(stderr, "Failed to open nmcli process");
+        exit(1);
+    }
+    const char *const fgets_result =
+        fgets(password_str, MAX_PASSWORD_LENGTH, nmcli_process);
+    // TODO: Remove the newline from fgets
+    if (fgets_result == NULL) {
+        fprintf(stderr, "Failed to read from nmcli process");
+        exit(1);
+    }
+    pclose(nmcli_process);
+}
+
 void fetch_wifi(void) {
     char ssid[MAX_SSID_LENGTH] = {0};
     get_ssid(ssid);
-    puts(ssid);
+    if (ssid[0] == '\0') {
+        fprintf(stderr, "No active ssid found. Aborting");
+        exit(1);
+    }
+    char password[MAX_PASSWORD_LENGTH] = {0};
+    get_password(password, ssid);
 }
 
 int main(int argc, char **argv) {
